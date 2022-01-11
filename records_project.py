0983f317c6d71ea6db5306911d4b1f4bad1f1fc0
Rina <93210033+Rinayas@users.noreply.github.com>
2022-01-11 13:03:18 +0200

    Update records_project.py

--- records_project.py
+++ records_project.py
@@ -8,15 +8,6 @@ Original file is located at
 """
 
 
-
-"""**Records Project**
-
-In this project I managed a collection of records.
-Each record has a unique name, and several copies (possibly more than a single copy).
-The software displays a text menu for the user and depending on the user's selection the actions will be performed. ⏭
-Project planning was through main functions. And some had sub-functions (auxiliary functions) according to the identification of a particular common behavior.⏺
-"""
-
 project_records = {"Hello " : 2
                    ,"Who run the world " : 1
                    ,"Big moon " : 100
